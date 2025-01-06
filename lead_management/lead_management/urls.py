0e31e9a3b958b2d631af959ba5a97fd149298840
WenDev <lwendel003@gmail.com>
2025-01-06 14:02:20 -0300

    .

--- lead_management/lead_management/urls.py
+++ lead_management/lead_management/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('leads.urls')),
+    path('', lambda request: JsonResponse({'message': 'Welcome to the Lead Management API!'})),
 ]
