4a7bbb980898fa07c8f0f0198de6f1b72d5f69b6
WenDev <lwendel003@gmail.com>
2025-01-06 14:18:22 -0300

    .

--- lead_management/lead_management/urls.py
+++ lead_management/lead_management/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.http import JsonResponse
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('leads.urls')),
-    path('', lambda request: JsonResponse({'message': 'Welcome to the Lead Management API!'})),
+    path('api/', include('leads.urls')),  # Rotas da API
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Documentação na raiz
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),  # Esquema OpenAPI
 ]
